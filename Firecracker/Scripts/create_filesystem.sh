ce0eb28fb3b4e38b0621b5eeccda8a23693af1d6
Ashenoy64 <ashenoy64@gmail.com>
2024-04-05 19:17:44 +0530

    Add Firecracker

---
+++ Firecracker/Scripts/create_filesystem.sh
@@ -0,0 +1,13 @@
+
+IMG_ID=firecracker
+CONTAINER_ID=$(docker run -td $IMG_ID /bin/bash)
+
+MOUNTDIR=mnt
+FS=mycontainer.ext4
+
+mkdir $MOUNTDIR
+qemu-img create -f raw $FS 800M
+mkfs.ext4 $FS
+mount $FS $MOUNTDIR
+docker cp $CONTAINER_ID:/ $MOUNTDIR
+umount $MOUNTDIR
